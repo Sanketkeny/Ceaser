897fcad900d65d011fc7302a46bf90919cde9937
Sanket keny <44596046+Sanketkeny@users.noreply.github.com>
2023-02-25 17:39:31 +0530

    Add files via upload

--- Ceaser.py
+++ Ceaser.py
@@ -1,2 +1,24 @@
-# Ceaser
-A python programme that converts normal text to Cipher Text
+#A python program to illustrate Caesar Cipher Technique
+def encrypt(text,s):
+	result = ""
+
+	# traverse text
+	for i in range(len(text)):
+		char = text[i]
+
+		# Encrypt uppercase characters
+		if (char.isupper()):
+			result += chr((ord(char) + s-65) % 26 + 65)
+
+		# Encrypt lowercase characters
+		else:
+			result += chr((ord(char) + s - 97) % 26 + 97)
+
+	return result
+
+#check the above function
+text = "SANKET"
+s = 7
+print ("Text : " + text)
+print ("Shift : " + str(s))
+print ("Cipher: " + encrypt(text,s))
